23a6db13b92951c7c2c22cf91cdf09b4eeeccabc
Md. Minhajul Islam <66138525+wrench786@users.noreply.github.com>
2021-03-22 17:53:55 +0600

    Added Vector-Sort.cpp
    Problem Link: https://www.hackerrank.com/challenges/vector-sort/problem

---
+++ Vector-Sort.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+
+    int n,i; cin>>n; vector<ll int>vec(n,0); for(i=0;i<n;i++) cin>>vec[i]; sort(vec.begin(),vec.end()); for(i=0;i<n;i++)cout<<vec[i]<<" "; cout<<endl;
+
+}
